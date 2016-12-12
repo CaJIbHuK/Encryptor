437251e3afd43e236671b1f79b86ac40c6215f5d
cajibhuk <antohazas@gmail.com>
2016-12-13 01:38:35 +0500

    new ver of libencFabric with RAII, 2DES, Memory Content Provider;

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 
 int main() {
 
-    Encryptor* encryptor = EncryptorFabric::getFileEncryptor(EncType::RC4,
+    std::shared_ptr<Encryptor> encryptor = EncryptorFabric::getFileEncryptor(EncType::DDES,
                                                              "/home/zas/Programming/C++/multyEnc/in",
                                                              "/home/zas/Programming/C++/multyEnc/enc",
                                                              "/home/zas/Programming/C++/multyEnc/key",
                                                              true);
     encryptor->encrypt();
 
-    Encryptor* decryptor = EncryptorFabric::getFileEncryptor(EncType::RC4,
+    std::shared_ptr<Encryptor> decryptor = EncryptorFabric::getFileEncryptor(EncType::DDES,
                                                              "/home/zas/Programming/C++/multyEnc/enc",
                                                              "/home/zas/Programming/C++/multyEnc/dec",
                                                              "/home/zas/Programming/C++/multyEnc/key");
